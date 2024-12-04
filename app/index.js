150923610fcf0b8ed764d98f84b1ba9a627a1f48
burakodaloglu <burakodaloglu1@gmail.com>
2024-12-04 22:36:52 +0300

    TextInput, Pressable, useState

--- app/index.js
+++ app/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router'; // useRouter kancasını import et
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
 
-export default function HomeScreen() {
-  const router = useRouter(); 
+export default function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [result, setResult] = useState("");
+
+  console.log(name);
+  console.log(lastName);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Button
-        title="View Details"
-        onPress={() => router.push('/details')}
-      ></Button>
+      <Text>Welcome {result}</Text>
+      <Text>Name</Text>
+      <TextInput
+        placeholder="Enter your Name"
+        style={styles.textInputStyle}
+        onChangeText={setName}
+        value={name}
+      />
+
+      <Text>Last Name</Text>
+      <TextInput
+        placeholder="Enter your Last Name"
+        style={styles.textInputStyle}
+        onChangeText={setLastName}
+        value={lastName}
+      />
+      <Pressable
+        onPress={() => setResult(name + " " + lastName)}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "gray" : "lightblue",
+          },
+          styles.button,
+        ]}
+      >
+        <Text style={styles.buttonText}>Save</Text>
+      </Pressable>
     </View>
   );
 }
@@ -19,12 +52,27 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
-  text: {
-    fontSize: 18,
-    marginBottom: 20,
+  textInputStyle: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    marginVertical: 10,
+    textAlign: "center",
+  },
+  button: {
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
